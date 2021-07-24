1f99b1be6d9ea773eba17d6e9e19201687d52042
James <james.ryan.mcguire@gmail.com>
2021-07-24 05:41:51 -0700

    Initial commit

---
+++ db/seeds.rb
@@ -0,0 +1,4 @@
+Place.create!([
+  {name: "Eiffel Tower", address: "Champ de Mars, 5 Av. Anatole France, 75007 Paris, France"},
+  {name: "Carson's Woodside Tavern", address: "57 NY-9P, Malta, NY 12020"}
+])
